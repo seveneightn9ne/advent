bc92b93c893d196678cf36899abafc6cdbc09e9f
Jess Kenney <seveneightn9ne@gmail.com>
2016-11-29 22:38:23 -0500

    december 1

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // TODO: calculate which day is today
+
+  // TODO: set the 'today' marker to today
+  $('li').click(function() {
+    // TODO: check if the clicked one is today or earlier
+
+    // TODO: something else blah
+  }
+
+});
